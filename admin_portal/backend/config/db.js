16dc84d10066cdce561482e60450c647f717ef96
Sharika <sharikasatheesh05@gmail.com>
2026-03-29 21:20:33 +0530

    Add default values to database connection settings
    Updated database connection configuration to include default values for host, port, user, password, and database.

--- admin_portal/backend/config/db.js
+++ admin_portal/backend/config/db.js
@@ -4,10 +4,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD, // Use password from .env
-    database: process.env.DB_NAME,
+    host: process.env.DB_HOST || 'gondola.proxy.rlwy.net',
+    port: process.env.DB_PORT || 20877, 
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'MMvKSmmQnvsGqntTwzwTrHXRduyFnRyv',
+    database: process.env.DB_NAME || 'railway',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
